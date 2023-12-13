e10603bb22e50b4d4e44c661ccb33b990401b5d2
Kishore Tamilselvan <kishoretamil14@gmail.com>
2023-12-13 19:25:17 +0530

    Initial commit

---
+++ src/main/java/com/ds/myapp/JenkinsDockerApplication.java
@@ -0,0 +1,12 @@
+package com.ds.myapp;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDockerApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
